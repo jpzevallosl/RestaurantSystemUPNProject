013f4cc91ecac6716e829ce3b2ba225c5c7d109e
jpzevallosl <74798550+jpzevallosl@users.noreply.github.com>
2022-04-16 12:08:02 -0500

    First upload
    Se agregan los primeros módulos de mesa, productos y usuarios

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using System.Collections;
+
+using System.Linq;
+
+using System.Text;
+
+
+
+
+
+namespace P5pilas
+
+{
+
+    internal class Program
+
+    {
+
+        public static int sumaNodos(IEnumerable myCollection)
+
+        {
+
+            int suma = 0;
+
+
+
+            foreach (Object obj in myCollection)
+
+                suma = suma + int.Parse(string.Format("{0}", obj));
+
+            return suma;
+
+        }
+
+
+
+        public static void Main()
+
+        {
+
+            Stack p5Pila = new Stack();
+
+            p5Pila.Push(5);
+
+            p5Pila.Pop();
+
+            p5Pila.Push(3);
+
+            p5Pila.Pop();
+
+            p5Pila.Push(7);
+
+            p5Pila.Push(3);
+
+            p5Pila.Push(1);
+
+
+
+
+
+            Console.Write("La suma de los datos de la pila es: " + sumaNodos(p5Pila) + " ");
+
+        }
+
+    }
+
+}
---
+++ mesa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sistemaRestaurante.modelo
+{
+    internal class mesa  //CLASE DONDE SE ASIGNARAN LOS DATOS PARA LA CREACION DE UNA MESA
+                         //EN EL SISTEMA
+    {
+        private int id;
+        private int numeroMesa;
+        private int idStatus;
+
+
+
+        //CONSTRUCTOR PARA INGRESAR Y OBTENER LOS DATOS SOLICITADOS DE LA CLASE MESA
+        public int Id   //INGRESAR Y OBTENER EL ID DE LA CLASE MESA
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int NumeroMesa   //INGRESAR Y OBTENER DEL NUMERO DE MESA DE LA CLASE MESA
+        {
+            get { return numeroMesa; }
+            set { numeroMesa = value; }
+        }
+        public int Idstatus    //INGRESAR Y OBTENER DEL STATUS  DE LA CLASE MESA
+        {
+            get { return idStatus; }
+            set { idStatus = value; }
+        }
+    }
+}
+
+
